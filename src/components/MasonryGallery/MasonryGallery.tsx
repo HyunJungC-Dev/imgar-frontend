37017c481c8709bae83fbfe4ca64d1289f454b8b
HyunJung <HyunJungC.Dev@gmail.com>
2021-11-27 15:25:19 +0900

    fix: Masnory Gallery 안 h2 누락 수정
    - ImageCard가 h3인데 Gallery를 의미하는 상위 h2가 없음을 확인 후 추가

--- src/components/MasonryGallery/MasonryGallery.tsx
+++ src/components/MasonryGallery/MasonryGallery.tsx
@@ -63,6 +63,7 @@ export default function MasonryGallery({ posts, id }: MasonryGalleryProps): Reac
 
   return (
     <>
+      <h2 className="a11y">Gallery</h2>
       <StyledSection ref={containerRef} id={id}>
         {posts.map((postInfo, index) => {
           const row = Math.floor(index / totalColumn);
