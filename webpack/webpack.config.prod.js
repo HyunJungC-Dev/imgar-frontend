e053c15c80a373d6b29dfedf3d21911cabb9044b
hyunjung <HyunJungC.Dev@gmail.com>
2021-12-03 14:44:33 +0900

    config: production 빌드 시,  console 제거 옵션 추가
    - production 시에는 테스트용인 console.log()가 빌드되지 않도록 설정

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.config.common');
 const path = require('path');
 const rootDir = process.cwd();
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(commonConfig, {
   mode: 'production',
@@ -9,6 +10,17 @@ module.exports = merge(commonConfig, {
   output: {
     path: path.resolve(rootDir, 'dist'),
     filename: '[name].[contenthash].bundle.js',
-    chunkFilename: '[name].[contenthash].chunk.js'
-  }
+    chunkFilename: '[name].[contenthash].chunk.js',
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+    ],
+  },
 });
